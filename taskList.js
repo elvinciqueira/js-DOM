d660983b83782a88af09110ea6e7665a367d5aa4
elvinciqueira <elvinciqueira@gmail.com>
2020-07-18 01:23:12 -0300

    feat: add new task

---
+++ taskList.js
@@ -0,0 +1,22 @@
+const newTask = document.querySelector("[data-form-button]");
+
+newTask.addEventListener("click", createTask);
+
+function createTask(event) {
+  event.preventDefault();
+
+  const list = document.querySelector("[data-list]");
+  const input = document.querySelector("[data-form-input]");
+  const value = input.value;
+
+  const task = document.createElement("li");
+  task.classList.add("task");
+
+  const content = `<p class="content">${value}</p>`;
+
+  task.innerHTML = content;
+
+  list.appendChild(task);
+
+  input.value = "";
+}
